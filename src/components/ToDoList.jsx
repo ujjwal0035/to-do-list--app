1c6786caaef24aff481eae674fac106294870844
Ujjwal <ujjwal0035@gmail.com>
2021-06-30 20:09:32 +0530

    fifth commit

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -8,12 +8,11 @@ const ToDoList =(props) =>{
 
     return (
         <div className="items-container">
-            {/* <label className="cross">x</label> */}
             <li>
-                <label className="cross" onClick={() =>props.onSelect(props.id)}>x</label>
+                <label className="delete cross"  onClick={() =>props.onSelect(props.id)}>x</label>
+                <label className="edit cross" onClick={() =>props.onSelectedit(props.id)}>edit</label>
                 {props.name}
                 </li>
-            
         </div>
     
     )
